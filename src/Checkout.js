ebb87d095393a2a3461d116c538cd20eba23d9a4
SIYOON JEON <siyoonjeon@SIYOONui-MacBookPro.local>
2021-06-21 21:24:06 +0900

    feat:basket 0621

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider';
 
 function Checkout(){
+  const [{basket}, dispatch] = useStateValue()
+  
   return(
     <div className='checkout'>
       <div className='ckeckout_left'>
@@ -14,7 +18,16 @@ function Checkout(){
               <h2 className='checkout_title'>
                 장바구니입니다
               </h2>
-              {/* 장바구니 아이템 */}
+              {basket.map(item => (
+                <CheckoutProduct 
+                  id={item.id} 
+                  title={item.title} 
+                  image={item.image} 
+                  price={item.price} 
+                  rating={item.rating}
+                />
+              ))}
+
             </div>
       </div>
 
