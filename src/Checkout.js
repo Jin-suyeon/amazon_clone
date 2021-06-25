bc912d15685af1e7783e548d42dc2e94c2a46853
SIYOON JEON <siyoonjeon@SIYOONui-MacBookPro.local>
2021-06-25 20:33:25 +0900

    feat:login ing

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutProduct from './CheckoutProduct'
 import { useStateValue } from './StateProvider';
 
 function Checkout(){
-  const [{basket}, dispatch] = useStateValue()
+  const [{basket, user}, dispatch] = useStateValue()
   
   return(
     <div className='checkout'>
@@ -16,7 +16,8 @@ function Checkout(){
 
             <div>
               <h2 className='checkout_title'>
-                장바구니입니다
+                {console.log(user)}
+                {user ?.email}의 장바구니입니다.
               </h2>
               {basket.map(item => (
                 <CheckoutProduct 
